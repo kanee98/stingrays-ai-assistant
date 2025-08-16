c0ef0f0348f17660ab3b215300dfd2b6f0f4065b
Kanishka Udapitiya <43329812+kanee98@users.noreply.github.com>
2025-08-16 20:03:15 +0530

    Authenticated WhatsApp server

---
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('whatsapp')
+export class WhatsappController {
+
+    @Get('webhook')
+    whatsappVerificationChallenge(@Req() request){
+        const mode = request.query['hub.mode'];
+        const challenge = request.query['hub.challenge'];
+        const token = request.query['hub.verify_token'];
+
+        const verificationToken = process.env.WHATSAPP_CLOUD_API_WEBHOOK_VERIFICATION_TOKEN;
+
+        if(!mode || !token){
+            return 'Error verifying token';
+        }
+
+        if(mode === 'subscribe' && token === verificationToken){
+            return challenge?.toString();
+        }
+    }
+}
