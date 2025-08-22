bba6352450b91fed095e72b4421b16cb85013c47
Kanishka Udapitiya <43329812+kanee98@users.noreply.github.com>
2025-08-22 22:53:11 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT || 3000;  
+  app.enableCors();
+  const port = parseInt(process.env.PORT ?? '3000', 10);
   await app.listen(port, '0.0.0.0');
+  console.log(`App running on port ${port}`);
 }
 bootstrap();
